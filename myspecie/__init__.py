1f8e01e63943346b207eeaf7db8465a8301e009c
LiuPhyMan <liu.jinbao@outlook.com>
2023-06-30 21:12:40 +0800

    Add Xe.

--- myspecie/__init__.py
+++ myspecie/__init__.py
@@ -10,30 +10,49 @@ import re
 import numpy as np
 from scipy.interpolate import interp1d
 from pandas import read_csv
-from myconst import relM2absM, eV2K, K2J, K2eV
+from myconst import relM2absM, eV2K, K2J, K2eV, absM2relM, m_e
 
-__all__ = ["spec_df", "SpecieWithLevel", "SpecieWithQint"]
+__all__ = ["spec_df", "SpecieWithLevel", "SpecieWithQint", "spc_dict"]
 
-spec_df = read_csv(__path__[0] + r"/specie_data.csv", sep=",", header=None, index_col=0, 
+spec_df = read_csv(__path__[0] + r"/specie_data.csv", sep=",", header=None, index_col=0,
                    names=["relM", "Zc", "Hf", "elems"], comment="#")
 
 
 class AbsSpecie(object):
 
-    def __init__(self, *, spc_str:str) -> None:
+    def __init__(self, *, spc_str: str) -> None:
         assert spc_str in spec_df.index, spc_str
         self.relM = spec_df.loc[spc_str, "relM"]
         self.absM = self.relM * relM2absM
         self.Zc = spec_df.loc[spc_str, "Zc"]
         self.Hf = spec_df.loc[spc_str, "Hf"]
-        self.elems = {_[0]:int(_[1]) for _ in re.findall(r"([a-zA-Z]+)-(\d+)", 
-                                                         spec_df.loc[spc_str, "elems"])}
+        self.elems = {_[0]: int(_[1]) for _ in re.findall(r"([a-zA-Z]+)-(\d+)",
+                                                          spec_df.loc[spc_str, "elems"])}
 
     def get_nElem(self, element: str):
-        return self.elems.get(element, 0)
+        if element == "e":
+            return -self.Zc
+        else:
+            return self.elems.get(element, 0)
 
     def qint(self, T_K):
         pass
+
+class _Electron(AbsSpecie):
+
+    def __init__(self) -> None:
+        super().__init__(spc_str="e")
+        self.elems = {"e": 1}
+
+    def qint(self, T_K):
+        return 2
+
+    def lnqint(self, T_K):
+        return log(2)
+
+    def rdcd_mu0(self, *, T_K):
+        r""" \mu(T) / kT  or \mu(T)/RT"""
+        return 3.66487052 - 1.5 * log(self.relM) - 2.5 * log(T_K) - log(2)
 
 class SpecieWithLevel(AbsSpecie):
 
@@ -58,13 +77,13 @@ class SpecieWithLevel(AbsSpecie):
 
     def dlnqintdT_2(self, T_K: float):
         T_eV = T_K * K2eV
-        temp = np.dot(self.g * self.E / T_eV * (-2 + self.E / T_eV), np.exp(-self.E / T_eV))
+        temp = np.dot(self.g * self.E / T_eV *
+                      (-2 + self.E / T_eV), np.exp(-self.E / T_eV))
         return (T_K * self.qint(T_K)) ** (-1) / T_K * temp
 
     def rdcd_mu0(self, *, T_K):
-        return -88.8291123098254 - 1.5 * log(self.absM) - 2.5 * log(T_K) - log(self.qint(T_K)) \
-            + self.Hf / (T_K * K2J)
-
+        return 3.66487052 - 1.5 * log(self.relM) - 2.5 * log(T_K) - log(self.qint(T_K)) \
+            + self.Hf * eV2K / T_K
 
 
 class SpecieWithQint(AbsSpecie):
@@ -90,30 +109,28 @@ class SpecieWithQint(AbsSpecie):
                                self._data[:, 4])(T_K) - 2 * T_K * dlnqdT) / T_K ** 2)
 
     def rdcd_mu0(self, *, T_K: float):
-        return -88.8291123098254 - 1.5 * log(self.absM) - 2.5 * log(T_K) - log(self.qint(T_K)) + \
-            self.Hf / (T_K * K2J)
+        return 3.66487052 - 1.5 * log(self.relM) - 2.5 * log(T_K) - log(self.qint(T_K)) + \
+            self.Hf * eV2K / T_K
+
 
 with open(__path__[0] + r"/spec_info.txt") as f:
     spec_info = f.readlines()
 
 spc_dict = dict()
-for _str in spec_info[:5]:
+spc_dict["e"] = _Electron()
+for _str in spec_info[:10]:
     _key, _type, _path = [_.strip() for _ in _str.split(",")]
     if _type == "level":
         spc_dict[_key] = SpecieWithLevel(spc_str=_key)
-        spc_dict[_key].set_gE(gE_file=f"{__path__[0]}/{_path}")
+        spc_dict[_key].set_gE(gE_file=f"{__path__[0]}/data/{_path}")
     elif _type == "qint":
         spc_dict[_key] = SpecieWithQint(spc_str=_key)
-        spc_dict[_key].set_qint(qint_file=f"{__path__[0]}/{_path}")
+        spc_dict[_key].set_qint(qint_file=f"{__path__[0]}/data/{_path}")
     else:
-        raise Exception(f"The type is error.")
-
-
-
+        raise Exception(f"The type {_type} is error.")
 
 
 # spc_dict = dict()
 # for _str in _spc_info:
 #     if _str[0] == "gE":
 #         spc_dict[_str[1]] = Specie
-
