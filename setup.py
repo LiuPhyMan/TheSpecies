8c08ff84d9d9f6c3c22af073d86c97340755bfbf
LiuPhyMan <liu.jinbao@outlook.com>
2024-02-01 05:37:48 +0800

    02.01.24

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ from setuptools import setup, find_packages
 setup(
     name = "myspecie",
     packages = find_packages(),
-    version = "0.2.0",
+    version = "1.0.0",
     install_requires=[
+        "numpy",
         "pandas",
+        "scipy",
         "myconst"
     ]
 )
